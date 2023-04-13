e7e8a29b7d85e9f90765fce90861021548842a22
andrewch8o.cld <andriy.chumachenko@cloudinary.com>
2023-04-13 16:37:00 -0700

    Formatted to make comment more readable

--- single-upload-from-url.js
+++ single-upload-from-url.js
@@ -2,11 +2,13 @@ const cloudinary = require('cloudinary').v2;
 
 /* 
 In this specific example we are using:
- - `use_filename: true` to instruct Cloudinary to use the file name of the asset being uploaded to produce asset public_id on upload
-	- `iss050e066094~orig` ⇒ `iss050e066094_orig` 
- - `unique_filename: false` to prevent Cloudinary from adding unique suffix to the public_id of the uploaded asset
+ * `use_filename: true` to instruct Cloudinary to use the file name of the asset being uploaded to produce asset public_id on upload
+	- `iss050e066094~orig` ⇒ `iss050e066094_orig`
+	
+ * `unique_filename: false` to prevent Cloudinary from adding unique suffix to the public_id of the uploaded asset
  	- This way public_id matches the original asset’s file name as closely as possible 
- - `asset_type: auto` to let the Cloudinary back-end determine the type of the asset ( `image`, `video` or `raw` ) on upload
+
+ * `asset_type: auto` to let the Cloudinary back-end determine the type of the asset ( `image`, `video` or `raw` ) on upload
  	- This way we do not need to maintain logic in the migration routine to determine type for each asset before upload 
 
  Note that in public_id cloudinary replaced `~` with `_`. More details on this behavior here:
